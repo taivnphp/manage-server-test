52d5a48bbe8935365658aed2e8512e3f127e1dca
TaiVN <taivn@elinext.com>
2016-09-27 17:27:34 +0700

    taivn update info new

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php 
-echo '123';
+use ElephantIO\Client as Elephant;
+
+$elephant = new Elephant('http://localhost:8000', 'socket.io', 1, false, true, true);
+
+$elephant->init();
+$elephant->send(
+    ElephantIOClient::TYPE_EVENT,
+    null,
+    null,
+    json_encode(array('name' => 'foo', 'args' => 'bar'))
+);
+$elephant->close();
+
+echo 'tryin to send `bar` to the event `foo`';
 ?>
